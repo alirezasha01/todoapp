d9278126bcf176a9d93be7826ca6c6747a35105b
Alireza <alirezasha01@gmail.com>
2023-01-30 22:50:12 +0330

    add input value to array

--- src/components/allTask.vue
+++ src/components/allTask.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="container-fluid">
-  <div class="row">
-    <div class="col"></div>
-    <div class="col-6">
-      <div class="input-group mb-3 mt-3 shadow-input">
-        <input type="text" class="form-control text-white bg-dark p-2" placeholder="Add a new task">
-        <button class="btn btn-success" type="button">ADD</button>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-md-6">
+        <div class="input-group mb-3 mt-3 shadow-input">
+          <input v-model="getTask" type="text" class="form-control text-white bg-dark p-2" placeholder="Add a new task">
+          <button @click="addTask" class="btn btn-success" type="button">ADD</button>
+        </div>
+        <p class="text-white">{{ listTask }}</p>
       </div>
+      <div class="col"></div>
     </div>
-    <div class="col"></div>
-  </div>
   </div>
 
 </template>
 
 <script setup>
+import {reactive, ref} from "vue";
 
+const getTask = ref("")
+const listTask = reactive([])
+
+const addTask = () => {
+  if (getTask.value !== "") {
+    listTask.push(getTask.value)
+    getTask.value = ""
+  }
+}
 </script>
 
 <style scoped>
@@ -23,10 +34,12 @@ input::placeholder {
   color: white;
   opacity: 1;
 }
-button{
+
+button {
   border: 1px white solid;
 }
-.shadow-input{
+
+.shadow-input {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
